ae5b6540bdf5cffbe7fc71f6173b4ab8d2cb0b3b
Granko15 <60289909+Granko15@users.noreply.github.com>
2026-09-21 08:01:14 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'hunt-v1';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
